0c6ffbab8cddcc0ef5615b826d1faa196606bf13
MarianaMilaniMatos <milani.mariana@yahoo.com.br>
2025-05-07 11:28:13 -0300

    atualização

--- Lógica de Programação/Atividades_Python/Ativ1_Ativ2/ativ2_dobro.py
+++
@@ -1,3 +0,0 @@
-numero= int(input("Digite um número inteiro: "))
-dobro= numero*2
-print(f"O dobro de {numero} é {dobro}.")
--- Lógica de Programação/Atividades_Python/Ativ1_Ativ2/atv2_tabuada.py
+++
@@ -1,9 +0,0 @@
-numero = int(input("Digite um número: "))
-
-for i in range(11):  # vai de 0 até 10
-    #in range sigifica que o loop vai de 0 até 10, ou seja, 11 números
-    #range = intervalo
-    #in range = dentro do intervalo
-    # para cada i dentro do intervalo de 0 a 10 faça:
-    # i = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-    print(f"{numero} x {i} = {numero * i}")
